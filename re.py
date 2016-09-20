b91e2b09211942fc42706dd416fed48e20d68431
RKolhatkar <rahul.kolhatkar96@gmail.com>
2016-09-20 08:06:01 +0530

    Added workaround for blank lines

--- re.py
+++ re.py
@@ -4,6 +4,6 @@ text = """Enter the text here. It will be shown as seperate sentences.
 """
 sentences = re.split(r'((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s)', text)
 for line in sentences:
-	if re.match(r'^\s*$',line):
-		continue
+	if re.match(r'^\s*$',line):	##To skip blank lines from op
+		continue		
 	print line
