de8cf5c114e8fa0d60be5b493a5582c13cecf0ef
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-09-20 10:35:49 +0530

    re: Read Input from file

--- re.py
+++ re.py
@@ -1,9 +1,12 @@
 import re
+import sys
 
-text = """Enter the text here. It will be shown as seperate sentences.
-"""
-sentences = re.split(r'((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s)', text)
+filepath=str(sys.argv[1])
+print filepath
+with open(filepath, 'r') as ifile:
+	s=ifile.read()
+sentences = re.split(r'((?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s)', s)
 for line in sentences:
-	if re.match(r'^\s*$',line):	##To skip blank lines from op
+	if re.match(r'^\s*$',line):	##To skip blank lines from op.
 		continue		
 	print line
